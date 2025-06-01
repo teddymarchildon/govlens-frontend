e0b6bfd55f24b45221845714669a5934792f41fc
teddymarchildon <ted.marchildon@gmail.com>
2025-06-01 17:30:46 -0400

    Rename congressmen to congressmembers 1

--- app/page.tsx
+++ app/page.tsx
@@ -334,7 +334,7 @@ function HomeContent() {
                     : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
                 }`}
               >
-                Congressmen ({savedCongressmen.length})
+                Congressmembers ({savedCongressmen.length})
               </button>
               <button
                 onClick={() => setActiveTab('judges')}
