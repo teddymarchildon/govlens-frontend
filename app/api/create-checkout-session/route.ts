76c8ba33e4ddc70f1d834a58b6aa8369cd17a7de
teddymarchildon <ted.marchildon@gmail.com>
2025-06-08 20:41:45 -0400

    Add biling

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -4,14 +4,14 @@ import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2025-05-28.basil' });
 
 export async function POST(req: NextRequest) {
-  const { userId } = await req.json();
+  const { userId, redirectUrl } = await req.json();
 
   try {
     const session = await stripe.checkout.sessions.create({
       mode: 'subscription',
       line_items: [{ price: 'price_1RXqViFHp5a6uQihQFgBJKBH', quantity: 1 }],
-      success_url: 'https://govsrc.com/profile',
-      cancel_url: 'https://govsrc.com/profile',
+      success_url: redirectUrl || 'https://govsrc.com/profile',
+      cancel_url: redirectUrl || 'https://govsrc.com/profile',
       client_reference_id: userId, // user's UUID
     });
 
